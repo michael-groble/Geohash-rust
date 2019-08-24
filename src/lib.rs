1e8ddc03a05b75825e850d9804c582bf64433679
michael groble <mike@groble.me>
2019-08-24 11:26:23 -0500

    get initial version of GeohashBits initialization working, without real implementations of interleaving the bits

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::f64;
+use std::f64::MAX;
 
 pub struct Location {
     pub longitude: f64,
@@ -10,10 +11,29 @@ pub struct BoundingBox {
     max: Location
 }
 
+#[derive(Clone, Copy)]
+pub enum Precision {
+    Bits(u8),
+    Characters(u8)
+}
+
+pub struct GeohashBits {
+    bits: u64,
+    precision: Precision
+}
+
 const RADIANS_PER_DEGREE: f64 = std::f64::consts::PI / 180.0;
 const EARTH_DISTANCE_METERS: f64 = 2.0 * 6_371_000.0;
+type LocationRange = std::ops::RangeInclusive<f64>;
+const LONGITUDE_RANGE: LocationRange = (-180.0..=180.0);
+const LATITUDE_RANGE: LocationRange = (-90.0..=90.0);
 
 impl Location {
+    pub fn validate_range(&self) {
+        assert!(LONGITUDE_RANGE.contains(&self.longitude), "longitude out of range");
+        assert!(LATITUDE_RANGE.contains(&self.latitude), "latitude out of range");
+    }
+
     pub fn distance_in_meters(&self, to: &Location) -> f64 {
         let self_lat = RADIANS_PER_DEGREE * self.latitude;
         let to_lat   = RADIANS_PER_DEGREE * to.latitude;
@@ -31,6 +51,8 @@ impl Location {
 
 impl BoundingBox {
     pub fn enclosing(a: &Location, b: &Location) -> BoundingBox {
+        a.validate_range();
+        b.validate_range();
         BoundingBox {
             min: Location {
                 latitude: a.latitude.min(b.latitude),
@@ -70,6 +92,51 @@ impl BoundingBox {
     }
 }
 
+const MAX_BINARY_PRECISION: u8 = 32;
+
+fn binary_precision(precision: &Precision) -> u8 {
+    match precision {
+        &Precision::Bits(n) => n,
+        &Precision::Characters(n) => (0.5 * (5 * n) as f32).ceil() as u8
+    }
+}
+
+fn max_binary_value(binary_precision: u8) -> f64 {
+    (1 << binary_precision as u64) as f64
+}
+
+fn float_to_bits(value: f64, range: &LocationRange, max_binary_value: f64) -> u32 {
+    let fraction = (value - *range.start()) / (range.end() - range.start());
+    (fraction * max_binary_value) as u32
+}
+
+fn bits_to_float(bits: u32, range: &LocationRange, max_binary_value: f64) -> f64 {
+    let fraction = (bits as f64) / max_binary_value;
+    *range.start() + fraction * (range.end() - range.start())
+}
+
+fn interleave_bits(even_bits: u32, odd_bits: u32) -> u64 {
+    (even_bits + odd_bits) as u64
+}
+
+impl GeohashBits {
+
+    pub fn from_location(location: &Location, precision: Precision) -> GeohashBits {
+        location.validate_range();
+        let binary_precision = binary_precision(&precision);
+        assert!((1..=MAX_BINARY_PRECISION).contains(&binary_precision), "precision out of range");
+        let max_binary_value = max_binary_value(binary_precision);
+
+        let longitude_bits = float_to_bits(location.longitude, &LONGITUDE_RANGE, max_binary_value);
+        let latitude_bits  = float_to_bits(location.latitude, &LATITUDE_RANGE, max_binary_value);
+
+        GeohashBits {
+            bits: interleave_bits(latitude_bits, longitude_bits),
+            precision
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use assert_approx_eq::assert_approx_eq;
@@ -82,6 +149,12 @@ mod tests {
         let b = Location { longitude: -9.11, latitude: 51.6 };
 
         assert_approx_eq!(a.distance_in_meters(&b), 11140.9, 0.1);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_validate_range() {
+        Location { longitude: 181.0, latitude: 0.0 }.validate_range();
     }
 
     fn bbox() -> BoundingBox {
